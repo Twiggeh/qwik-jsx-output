f0657f1bf1ed401bb4aa4ffcf3d30598015e2cee
albert <twiggeh99@gmail.com>
2024-05-25 12:54:36 +0200

    fixed inlined component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,102 +1,100 @@
 import type { JSXOutput } from "@builder.io/qwik";
 import {
-	component$,
-	useSignal,
-	useTask$,
-	useVisibleTask$,
+  component$,
+  useSignal,
+  useTask$,
+  useVisibleTask$,
 } from "@builder.io/qwik";
 import { server$, routeLoader$ } from "@builder.io/qwik-city";
 import { isServer } from "@builder.io/qwik/build";
 
 declare global {
-	// eslint-disable-next-line
-	var rooms: string[] | undefined;
+  // eslint-disable-next-line
+  var rooms: string[] | undefined;
 }
 
 const _getRooms = () => {
-	if (!isServer) throw new Error("No access from client");
-	if (!globalThis.rooms)
-		globalThis.rooms = ["hello", "goodbye", "something else", "noooo"];
-	return globalThis.rooms as string[];
+  if (!isServer) throw new Error("No access from client");
+  if (!globalThis.rooms)
+    globalThis.rooms = ["hello", "goodbye", "something else", "noooo"];
+  return globalThis.rooms as string[];
 };
 
 const ExpComp = component$(({ rooms }: { rooms: string[] }) => {
-	if (!rooms.length) return <div>No Rooms yet!</div>;
+  if (!rooms.length) return <div>No Rooms yet!</div>;
 
-	return (
-		<div>
-			{rooms.map((g) => (
-				<div key={g}>{g}</div>
-			))}
-		</div>
-	);
+  return (
+    <div>
+      {rooms.map((g) => (
+        <div key={g}>{g}</div>
+      ))}
+    </div>
+  );
 });
 
-const getRooms = server$(() => _getRooms());
-
 const getMyExpensive = server$(() => {
-	const rooms = _getRooms();
-	return <ExpComp rooms={rooms}></ExpComp>;
+  const rooms = _getRooms();
+  return <ExpComp rooms={rooms}></ExpComp>;
+});
+
+const getMyExpensive2 = server$(() => {
+  const rooms = _getRooms();
+
+  if (!rooms.length) return <div>No Rooms yet!</div>;
+  return (
+    <div>
+      {rooms.map((g) => (
+        <div key={g}>{g}</div>
+      ))}
+    </div>
+  );
 });
 
 export const useRoomsLoader = routeLoader$(() => {
-	return getMyExpensive();
+  return getMyExpensive();
 });
 
 export default component$(() => {
-	const rooms = useRoomsLoader();
-	const mySigTask = useSignal<JSXOutput>();
-	const mySigVisible = useSignal<JSXOutput>();
-	const mySigVisible2 = useSignal<JSXOutput>();
-	const load = useSignal(0);
+  const rooms = useRoomsLoader();
+  const mySigTask = useSignal<JSXOutput>();
+  const mySigVisible = useSignal<JSXOutput>();
+  const mySigVisible2 = useSignal<JSXOutput>();
+  const load = useSignal(0);
 
-	useTask$(async (ctx) => {
-		ctx.track(() => load.value);
-		mySigTask.value = await getMyExpensive();
-	});
+  useTask$(async (ctx) => {
+    ctx.track(() => load.value);
+    mySigTask.value = await getMyExpensive();
+  });
 
-	// eslint-disable-next-line
-	useVisibleTask$(async () => {
-		mySigVisible.value = await getMyExpensive();
-	});
+  // eslint-disable-next-line
+  useVisibleTask$(async () => {
+    mySigVisible.value = await getMyExpensive();
+  });
 
-	// eslint-disable-next-line
-	useVisibleTask$(async () => {
-		const rooms = await getRooms();
+  // eslint-disable-next-line
+  useVisibleTask$(async () => {
+    mySigVisible2.value = await getMyExpensive2();
+  });
 
-		if (!rooms.length) mySigVisible2.value = <div>No Rooms yet!</div>;
-
-		mySigVisible2.value = (
-			<div>
-				{rooms.map((g) => (
-					<div key={g}>{g}</div>
-				))}
-			</div>
-		);
-		return;
-	});
-
-	return (
-		<>
-			<div style={{ border: "solid 1px red" }}>
-				server$ + useTask:
-				{mySigTask.value}
-			</div>
-			<button onClick$={() => (load.value += 1)}>break! (check console)</button>
-			<div style={{ border: "solid 1px hotpink" }}>
-				server$ + useVisibleTask:
-				{mySigVisible.value}
-			</div>
-
-			<div style={{ border: "solid 1px blue" }}>
-				server$ + useVisibleTask + no component:
-				{mySigVisible2.value}
-			</div>
-
-			<div style={{ border: "solid 1px green" }}>
-				useRouteLoader:
-				{rooms.value}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div style={{ border: "solid 1px red" }}>
+        server$ + useTask:
+        {mySigTask.value}
+      </div>
+      <button onClick$={() => (load.value += 1)}>break! (check console)</button>
+      <div style={{ border: "solid 1px hotpink" }}>
+        server$ + useVisibleTask:
+        {mySigVisible.value}
+      </div>
+      <div style={{ border: "solid 1px blue" }}>
+        server$ + useVisibleTask + no component:
+        {mySigVisible2.value}
+      </div>
+      <div style={{ border: "solid 1px green" }}>
+        useRouteLoader:
+        {rooms.value}
+      </div>
+    </>
+  );
 });
